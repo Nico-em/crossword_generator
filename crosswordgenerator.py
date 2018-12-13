edab64a5486d2ddd9e8b3990d30919a38221ded1
Em.ma <grottoemma@gmail.com>
2018-12-13 13:11:08 -0500

    updated version to github

--- crosswordgenerator.py
+++ crosswordgenerator.py
@@ -53,7 +53,7 @@ class CrosswordGenerator:
             return None
 
         index = 0
-        while( word_size+3 >= 3):
+        while(word_size+3 >= 3):
             for i in self.words[word_size][1]:
                 word = list(i.keys())[0]
                 flag = True
@@ -68,13 +68,13 @@ class CrosswordGenerator:
                             break
                 if flag == True:
                     # del self.words[variable[1]-3][1][0][word]
-                    print(word)
+                    print(self.words[word_size][1][0])
+                    del self.words[word_size][1][0]
                     return word
             word_size -= 1
 
-        print(word)
-        # print(self.words[variable[1]-3][1][0][word])
-        # del self.words[variable[1]-3][1][0][word]
+        # print(self.words[word_size][1][0])
+        # del self.words[word_size][1][0]
         return None
 
     # get the starting position for word and max size
@@ -84,37 +84,103 @@ class CrosswordGenerator:
             for col in range(len(sol)):
                 pos = (row, col)
 
-                #check constriants
-                # if direction == HORIZONTAL:
-                #     if sol[row][col] != " ":
-                #         return [pos, self.size - pos[1]]
-                #     elif (row - 1 != 0 and sol[row-1][col] != " ") or (row + 1 < len(sol) and sol[row+1][col] != " "):
-                #         print("here")
-                #         continue
-                #     else:
-                #         return [pos, self.size - pos[1]]
-                # else:
-                #     if sol[row][col] != " ":
-                #         return [pos, self.size - pos[1]]
-                #     elif (col - 1 != 0 and sol[row][col-1] != " ") or (col + 1 < len(sol) and sol[row][col+1] != " "):
-                #         continue
-                #     else:
-                #         return [pos, self.size - pos[1]]
                 if direction == HORIZONTAL:
-                    if (row - 1 != 0 and sol[row-1][col] != " ") or (row + 1 < len(sol) and sol[row+1][col] != " "):
-                        continue
-                    else:
-                        return [pos, self.size - pos[1]]
-                else:
-                    if (col - 1 != 0 and sol[row][col-1] != " ") or (col + 1 < len(sol) and sol[row][col+1] != " "):
-                        continue
-                    else:
-                        return [pos, self.size - pos[0]]
+                    if sol[row][col] == " ":
+                        if row == 0:
+                            if sol[row+1][col] != " ":
+                                continue
+                        elif row == self.size-1:
+                            if sol[row-1][col] != " ":
+                                continue
+                        else:
+                            if(sol[row-1][col] != " " and sol[row+1][col]):
+                                continue
 
-        if (direction == HORIZONTAL):
-            return [pos, self.size - pos[1]]
-        else:
-            return [pos, self.size - pos[0]]
+                    if(col == 0):
+                        if(sol[row][col+1] != " "):
+                            continue
+                    elif(col == self.size-1):
+                        if(sol[row][col-1] != " "):
+                            continue
+                    else:
+                        if (sol[row][col-1] != " ") and (sol[row][col+1] != " "):
+                            continue
+
+                    if(col == self.size-1):
+                        continue
+                    for i in range(col+1, self.size):
+                        if(sol[row][i] != " "):
+                            s = i - col - 1
+                            if s < 3:
+                                break
+                            return [pos, s]
+                        elif row == 0:
+                            if sol[row+1][i] != " ":
+                                s = i - col
+                                if s < 3:
+                                    break
+                                return [pos, s]
+                        elif row == self.size-1:
+                            if sol[row-1][i] != " ":
+                                s = i - col
+                                if s < 3:
+                                    break
+                                return [pos, s]
+                        else:
+                            if sol[row-1][i] != " " or sol[row+1][i]:
+                                s = i - col
+                                if s < 3:
+                                    break
+                                return [pos, s]
+                else:
+                #vertical
+                    if sol[row][col] == " ":
+                        if col == 0:
+                            if sol[row][col+1] != " ":
+                                continue
+                        elif col == self.size-1:
+                            if sol[row][col-1] != " ":
+                                continue
+                        else:
+                            if(sol[row][col-1] != " " and sol[row][col+1]):
+                                continue
+
+                    if(row == 0):
+                        if(sol[row+1][col] != " "):
+                            continue
+                    elif(row == self.size-1):
+                        if(sol[row-1][col] != " "):
+                            continue
+                    else:
+                        if (sol[row-1][col] != " ") and (sol[row+1][col] != " "):
+                            continue
+
+                    if(row == self.size-1):
+                        continue
+                    for i in range(row+1, self.size):
+                        if(sol[i][col] != " "):
+                            s = i - row - 1
+                            if s < 3:
+                                break
+                            return [pos, s]
+                        elif col == 0:
+                            if sol[i][col+1] != " ":
+                                s = i - row
+                                if s < 3:
+                                    break
+                                return [pos, s]
+                        elif col == self.size-1:
+                            if sol[i][col-1] != " ":
+                                s = i - row
+                                if s < 3:
+                                    break
+                                return [pos, s]
+                        else:
+                            if sol[i][col-1] != " " or sol[i][col+1]:
+                                s = i - row
+                                if s < 3:
+                                    break
+                                return [pos, s]
 
 
 
