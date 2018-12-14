36377a91b041c8a57c282394acfaf11b07ef3751
igriffin415 <fireice16@yahoo.com>
2018-12-13 20:27:04 -0500

    multi-run and math

--- evaluate.py
+++ evaluate.py
@@ -37,10 +37,39 @@ words = load()
 wordsSet = set([])
 words = shuffle(words)
 
+data = list(range(3,16))
+subdata = []
+
+num_runs = 20
+
 for size in range(3,16):
-    start = time.time()
-    crossword_problem = CrosswordGenerator(size, words, wordsSet)
-    solution = crossword_problem.backtracking()
-    end = time.time()
-    perc_filled = space_filled(solution)
-    print("\nsize:",size, "\ttime:",end - start, "s\tpercent filled:",perc_filled)
+    subdata = []
+    for i in range(num_runs):
+        start = time.time()
+        crossword_problem = CrosswordGenerator(size, words, wordsSet)
+        solution = crossword_problem.backtracking()
+        end = time.time()
+        perc_filled = space_filled(solution)
+        # time = end - start
+        # print("\nsize:",size, "\ttime:",end - start, "s\tpercent filled:",perc_filled)
+        subdata.append([end - start, perc_filled, size])
+    data[size-3] = subdata
+
+p_size = 3
+
+# for list in data:
+#     print(list)
+    # for individual in list:
+    #     print(individual)
+
+for list in data:
+    perc_filled = 0
+    time = 0
+    # print(list, "\n")
+    for individual in list:
+        # print(individual)
+        time += individual[0]
+        perc_filled += individual[1]
+
+    print("\nsize:",p_size,"\ttime:",time/num_runs, "s\tpercent filled:",perc_filled/num_runs)
+    p_size+= 1
